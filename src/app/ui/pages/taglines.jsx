776fe7acf81f5174c641f83d48eac9e01121ec84
Gracia Juniora <gjuniora.vllnv27@gmail.com>
2025-03-26 15:03:55 +0800

    Update images

--- src/app/ui/pages/taglines.jsx
+++ src/app/ui/pages/taglines.jsx
@@ -5,7 +5,7 @@ export default function TaglinPages() {
     <>
       {/* Student Tagline */}
       <section className="flex flex-col md:flex-row-reverse items-center justify-center gap-12 px-6 sm:px-12 md:px-20 py-12 bg-[#FFFFFF] relative"> 
-        <div className="grid grid-cols-2 gap-4 w-full md:w-1/2">
+      <div className="grid grid-cols-2 gap-4 w-full md:w-1/2 ">
           <div className="col-span-1 row-span-2">
             <Image
               src="/images/IMG_15.JPG"
@@ -13,7 +13,7 @@ export default function TaglinPages() {
               width={400}
               height={500}
               quality={100}
-              className=" object-cover w-full h-full"
+              className="rounded-xl object-cover w-full h-full relative z-10"
             />
           </div>
           <Image
@@ -22,7 +22,7 @@ export default function TaglinPages() {
             width={200}
             height={200}
             quality={100}
-            className="rounded-xl object-cover w-full"
+            className="rounded-xl object-cover w-full h-full relative z-10"
           />
           <Image
             src="/images/IMG_5.JPG"
@@ -30,7 +30,7 @@ export default function TaglinPages() {
             width={200}
             height={200}
             quality={100}
-            className="rounded-xl object-cover w-full"
+            className="rounded-xl object-cover w-full h-full relative z-10"
           />
         </div>
         <div className=" p-6 lg:p-10  max-w-md">
@@ -48,33 +48,33 @@ export default function TaglinPages() {
       {/* Companies */}
       <section className="flex flex-col md:flex-row items-center justify-center gap-8 px-6 py-12 bg-[#FFFFFF] relative">
       <div className="grid grid-cols-2 gap-4 w-full md:w-1/2 ">
-        <div className="col-span-1 row-span-2">
+          <div className="col-span-1 row-span-2">
+            <Image
+              src="/images/IMG_14.JPG"
+              alt="Person with Laptop"
+              width={400}
+              height={500}
+              quality={100}
+              className="rounded-xl object-cover w-full h-full relative z-10"
+            />
+          </div>
           <Image
-            src="/images/IMG_14.JPG"
-            alt="Person with Laptop"
-            width={400}
-            height={500}
+            src="/images/IMG_10.JPG"
+            alt="Tablet Screen"
+            width={200}
+            height={200}
             quality={100}
-            className="rounded-xl object-cover w-full h-full"
+            className="rounded-xl object-cover w-full h-full relative z-10"
+          />
+          <Image
+            src="/images/IMG_3.JPG"
+            alt="Team Discussion"
+            width={200}
+            height={200}
+            quality={100}
+            className="rounded-xl object-cover w-full h-full relative z-10"
           />
         </div>
-        <Image
-          src="/images/IMG_10.JPG"
-          alt="Tablet Screen"
-          width={200}
-          height={200}
-          quality={100}
-          className="rounded-xl object-cover w-full"
-        />
-        <Image
-          src="/images/IMG_3.JPG"
-          alt="Team Discussion"
-          width={200}
-          height={200}
-          quality={100}
-          className="rounded-xl object-cover w-full"
-        />
-      </div>
       
       {/* <div className="absolute bottom-40 right-0 w-70 h-70 bg-orange-200 rounded-full hidden md:block z-0"></div>  */}
       <div className="p-6 lg:p-10  max-w-md" >
