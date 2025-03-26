79d5f59fd586020260d6962bb7008ee1ba36a3f9
Jdexurbano <dexterurbano25@gmail.com>
2025-03-26 22:56:04 +0800

    fix format issues. fix the form and required to each field.

--- src/app/ui/pages/pre-register.jsx
+++ src/app/ui/pages/pre-register.jsx
@@ -18,56 +18,52 @@ export default function PreRegisterPage() {
   ];
 
   return (
-
-    <>
-      <section
-        id="registerPage"
-
-        className="bg-[#FFFFFF] w-full grid gap-10 py-5 px-7
+    <section
+      id="registerPage"
+      className="bg-[#FFFFFF] w-full grid gap-10 py-5 px-7
         md:grid-cols-2 md:px-16 md:py-14"
-      >
-        <div className="grid grid-cols-2 items-center">
-          
-          <Image
-            src="/images/img_10.png"
-            width={360}
-            height={920}
-            alt="latina"
-          />
-          <div className="grid grid-cols-1 gap-9 lg:gap-20">
-            {iconData.map((data) => {
-              return (
-                <div
-                  key={data.id}
-                  className={`flex justify-start items-center ${
-                    data.id === 1 ? "-ml-4.5" : data.id === 3 ? "ml-2" : ""
-                  }`}
-                >
-                  <div className="bg-[#135C9F] w-16 h-16 flex justify-center items-center rounded-full z-40">
-                    <img
-                      src={`/svg/${data.img}.svg`}
-                      alt=""
-                      className="w-12 h-12"
-                    />
-                  </div>
-                  <div className="bg-white h-14 -ml-2 flex justify-start items-center w-50 rounded-r-lg">
-                    <p className="text-slate-700 font-semibold ml-5">
-                    {data.description}
-                    </p>
-                  </div>
+    >
+      <div className="grid grid-cols-2 items-center">
+        <Image src="/images/img_10.png" width={360} height={920} alt="latina" />
+        <div className="grid grid-cols-1 gap-9 lg:gap-20">
+          {iconData.map((data) => {
+            return (
+              <div
+                key={data.id}
+                className={`flex justify-start items-center ${
+                  data.id === 1 ? "-ml-4.5" : data.id === 3 ? "ml-2" : ""
+                }`}
+              >
+                <div className="bg-[#135C9F] w-16 h-16 flex justify-center items-center rounded-full z-40">
+                  <img
+                    src={`/svg/${data.img}.svg`}
+                    alt=""
+                    className="w-12 h-12"
+                  />
                 </div>
-              );
-            })}
-          </div>
-
+                <div className="bg-white h-14 -ml-2 flex justify-start items-center w-50 rounded-r-lg">
+                  <p className="text-slate-700 font-semibold ml-5">
+                    {data.description}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
 
       {/* FORM */}
       <div className="px-8 rounded-lg">
         <div className="bg-[#F3F3F3] rounded-2xl w-full h-full p-7 py-3 md:px-14 md:py-14">
-          <h1 className="font-bold text-2xl text-slate-400 py-3">Pre-register</h1>
-          <Image src="/images/LOGO_blue_png.png" width={281} height={53} alt="logo" />
+          <h1 className="font-bold text-2xl text-slate-400 py-3">
+            Pre-register
+          </h1>
+          <Image
+            src="/images/LOGO_blue_png.png"
+            width={281}
+            height={53}
+            alt="logo"
+          />
 
           {/* Dropdown with White Background */}
           <select
@@ -84,38 +80,49 @@ export default function PreRegisterPage() {
 
           {/* Company Form - Only Shows When 'Company' is Selected */}
           {role === "company" && (
-            <div className="mt-4">
+            <form
+              className="mt-4"
+              method="POST"
+              onSubmit={handleSubmit}
+              id="company_form"
+            >
               <input
+                required
                 type="text"
                 placeholder="Company Name"
                 className="w-full p-3 border rounded-md mt-2 bg-white text-gray-700"
               />
               <input
+                required
                 type="email"
                 placeholder="Company Email"
                 className="w-full p-3 border rounded-md mt-2 bg-white text-gray-700"
               />
               <input
+                required
                 type="text"
                 placeholder="Company Address"
                 className="w-full p-3 border rounded-md mt-2 bg-white text-gray-700"
               />
               <input
+                required
                 type="text"
                 placeholder="Contact Person"
                 className="w-full p-3 border rounded-md mt-2 bg-white text-gray-700"
               />
               <input
-                type="number"
+                required
+                type="text"
                 placeholder="Contact Number"
                 className="w-full p-3 border rounded-md mt-2 bg-white text-gray-700"
               />
-            </div>
+            </form>
           )}
 
           {/* Submit Button */}
           <button
-            onClick={handleSubmit}
+            type="submit"
+            form="company_form"
             className="mt-6 bg-[#135C9F] text-white px-6 py-3 rounded-md text-sm font-medium shadow-md hover:bg-blue-700 cursor-pointer w-full"
           >
             Pre-register Now
@@ -128,7 +135,9 @@ export default function PreRegisterPage() {
         <div className="fixed inset-0 flex items-center justify-center bg-blue bg-opacity-50">
           <div className="bg-white p-6 shadow-lg rounded-lg border border-gray-300 text-center relative">
             <h3 className="text-lg font-semibold text-green-600">Success!</h3>
-            <p className="mt-2 text-gray-700">Your registration has been successfully submitted.</p>
+            <p className="mt-2 text-gray-700">
+              Your registration has been successfully submitted.
+            </p>
             <button
               onClick={() => setIsSubmitted(false)}
               className="mt-4 bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg"
