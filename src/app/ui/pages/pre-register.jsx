7d04ff535247b9465160f59faec5166e906cc070
Jdexurbano <dexterurbano25@gmail.com>
2025-03-26 23:23:33 +0800

    fix merge issues

--- src/app/ui/pages/pre-register.jsx
+++ src/app/ui/pages/pre-register.jsx
@@ -5,7 +5,7 @@ export default function PreRegisterPage() {
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [role, setRole] = useState(""); // Dropdown state
   const [scchool_id, setschool_id] = useState(""); // Dropdown state
-  const [Year_lvl,setYear_lvl] = useState("");
+  const [Year_lvl, setYear_lvl] = useState("");
 
   const iconData = [
     { id: 1, img: "work", description: "Access to Fresh Talents" },
@@ -16,35 +16,32 @@ export default function PreRegisterPage() {
   return (
     <section
       id="registerPage"
-      className="bg-[#FFFFFF] w-full grid gap-10 py-5 px-7
-        md:grid-cols-2 md:px-16 md:py-14"
+      className="bg-[#FFFFFF] w-full grid gap-10 py-5 px-7 md:grid-cols-2 md:px-16 md:py-14"
     >
       <div className="grid grid-cols-2 items-center">
         <Image src="/images/img_10.png" width={360} height={920} alt="latina" />
         <div className="grid grid-cols-1 gap-9 lg:gap-20">
-          {iconData.map((data) => {
-            return (
-              <div
-                key={data.id}
-                className={`flex justify-start items-center ${
-                  data.id === 1 ? "-ml-4.5" : data.id === 3 ? "ml-2" : ""
-                }`}
-              >
-                <div className="bg-[#135C9F] w-16 h-16 flex justify-center items-center rounded-full z-40">
-                  <img
-                    src={`/svg/${data.img}.svg`}
-                    alt=""
-                    className="w-12 h-12"
-                  />
-                </div>
-                <div className="bg-white h-14 -ml-2 flex justify-start items-center w-50 rounded-r-lg">
-                  <p className="text-slate-700 font-semibold ml-5">
-                    {data.description}
-                  </p>
-                </div>
+          {iconData.map((data) => (
+            <div
+              key={data.id}
+              className={`flex justify-start items-center ${
+                data.id === 1 ? "-ml-4.5" : data.id === 3 ? "ml-2" : ""
+              }`}
+            >
+              <div className="bg-[#135C9F] w-16 h-16 flex justify-center items-center rounded-full z-40">
+                <img
+                  src={`/svg/${data.img}.svg`}
+                  alt=""
+                  className="w-12 h-12"
+                />
               </div>
-            );
-          })}
+              <div className="bg-white h-14 -ml-2 flex justify-start items-center w-50 rounded-r-lg">
+                <p className="text-slate-700 font-semibold ml-5">
+                  {data.description}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
 
@@ -73,13 +70,12 @@ export default function PreRegisterPage() {
             <option value="company">Company</option>
           </select>
 
-
           {/* FORM STARTS HERE */}
           <form
             action="https://docs.google.com/forms/u/2/d/e/1FAIpQLSfDi7cmdoYhuU5S1r2KNHgSmBTH6-TBxSYBjt_pXbZw362aFA/formResponse"
             method="POST"
             target="_blank"
-            onSubmit={() => setIsSubmitted(true)} // Show success message after submission
+            onSubmit={() => setIsSubmitted(true)}
           >
             {/* Company Form - Only Shows When 'Company' is Selected */}
             {role === "company" && (
@@ -125,75 +121,73 @@ export default function PreRegisterPage() {
             {/* Student Form */}
             {role === "student" && (
               <div className="mt-5">
-          <select
-            id="school_id"
-            className="w-full p-3 border rounded-md mt-2 focus:ring-blue-500 focus:border-blue-500 bg-white text-gray-700"
-            value={scchool_id}
-            onChange={(e) => setschool_id(e.target.value)}
-          >
-            <option value="">Select School</option>
-            <option value="student">NCF</option>
-          </select>
+                <select
+                  id="school_id"
+                  className="w-full p-3 border rounded-md mt-2 focus:ring-blue-500 focus:border-blue-500 bg-white text-gray-700"
+                  value={scchool_id}
+                  onChange={(e) => setschool_id(e.target.value)}
+                >
+                  <option value="">Select School</option>
+                  <option value="NCF">NCF</option>
+                </select>
                 <input
                   name="entry.1361204234"
                   type="text"
                   placeholder="First Name"
                   className="w-full p-3 border rounded-md mt-2 bg-white text-gray-700"
                   required
-                  />
-                  <input
+                />
+                <input
                   name="entry.1361204234"
                   type="text"
                   placeholder="Course"
                   className="w-full p-3 border rounded-md mt-2 bg-white text-gray-700"
                   required
-                  />
-           <select
-            id="Year_lvl"
-            className="w-full p-3 border rounded-md mt-2 focus:ring-blue-500 focus:border-blue-500 bg-white text-gray-700"
-            value={Year_lvl}
-            onChange={(e) => setYear_lvl(e.target.value)}
-=======
-         
-          >
-            <option value="">Year Level </option>
-            <option value="1styr">1st Year</option>
-            <option value="2ndyr">2nd Year</option>
-            <option value="3rdyr">3rd Year</option>
-            <option value="4thyr">4th Year</option>
-          </select>
-          <input
+                />
+                <select
+                  id="Year_lvl"
+                  className="w-full p-3 border rounded-md mt-2 focus:ring-blue-500 focus:border-blue-500 bg-white text-gray-700"
+                  value={Year_lvl}
+                  onChange={(e) => setYear_lvl(e.target.value)}
+                >
+                  <option value="">Year Level</option>
+                  <option value="1styr">1st Year</option>
+                  <option value="2ndyr">2nd Year</option>
+                  <option value="3rdyr">3rd Year</option>
+                  <option value="4thyr">4th Year</option>
+                </select>
+                <input
                   name="entry.1361204234"
                   type="text"
                   placeholder="School Email"
                   className="w-full p-3 border rounded-md mt-2 bg-white text-gray-700"
                   required
-                  />
-          <input
+                />
+                <input
                   name="entry.1361204234"
                   type="text"
                   placeholder="Current Town"
                   className="w-full p-3 border rounded-md mt-2 bg-white text-gray-700"
                   required
-                  />
-          <input
+                />
+                <input
                   name="entry.1361204234"
-                  type="number"
+                  type="text"
                   placeholder="Contact Number"
                   className="w-full p-3 border rounded-md mt-2 bg-white text-gray-700"
                   required
-                  />
-          <input
+                />
+                <input
                   name="entry.1361204234"
                   type="text"
                   placeholder="Target Industry"
-                  className="w-full p-10 border rounded-md mt-2 bg-white text-gray-700"
+                  className="w-full p-3 border rounded-md mt-2 bg-white text-gray-700"
                   required
-                  />
-           </div>
+                />
+              </div>
             )}
 
-            {/* Submit Button (Now Inside the Form) */}
+            {/* Submit Button */}
             <button
               type="submit"
               className="mt-6 bg-[#135C9F] text-white px-6 py-3 rounded-md text-sm font-medium shadow-md hover:bg-blue-700 cursor-pointer w-full"
